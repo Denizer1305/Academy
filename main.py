e5be9d8303c7d6e99fbeb6007ef31221f2c0067d
TopAcademy <dfghj@sdjh.com>
2024-02-13 21:32:13 +0300

    first commit: ничего не понял, но было очень интересно

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
 
-menu = ["Главная", "Галерея", "О нас"]
+menu = [{"name": "Главная", "url": "/"},
+        {"name": "О нас", "url": "/about"},]
 
 
 @app.route("/")
 @app.route("/index")
 def index():
+    print(url_for('index'))
     return render_template("index.html", menu=menu, title="Главгая страница")
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html", title="Страница о компании")
+    print(url_for('about'))
+    return render_template("about.html",menu=menu, title="Страница о компании")
+
+
+@app.route("/profile/<username>")
+def profile(username):
+    print(url_for('profile', username="11"))
+    return f"Пользователь -- {username}"
 
 
 if __name__ == "__main__":
