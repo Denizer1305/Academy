a1cd46b071054596af09cb750268201bf8f831b2
TopAcademy <dfghj@sdjh.com>
2024-02-16 20:02:39 +0300

    123

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
 
+
+app.config["SECRET_KEY"] = "3SAgu6jDn-u3x6nIEPmf5lTS5"
 menu = [{"name": "Главная", "url": "/"},
-        {"name": "О нас", "url": "/about"},]
+        {"name": "О нас", "url": "/about"},
+        {"name": "Обратная связь", "url": "/contact"}, ]
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    print(url_for('index'))
     return render_template("index.html", menu=menu, title="Главгая страница")
 
 
 @app.route("/about")
 def about():
-    print(url_for('about'))
-    return render_template("about.html",menu=menu, title="Страница о компании")
+    return render_template("about.html", menu=menu, title="Страница о компании")
 
 
 @app.route("/profile/<username>")
 def profile(username):
-    print(url_for('profile', username="11"))
     return f"Пользователь -- {username}"
+
+
+@app.route('/contact', methods=["POST", "GET"])
+def contact():
+    if request.method == 'POST':
+        if len(request.form['name']) >= 2:
+            flash("Сообщение доставлено", category="success")
+        else:
+            flash("Ошибка!", category="error")
+    return render_template("contact.html", menu=menu, title="Обратная связь")
+
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template("page404.html", menu=menu, title="Страница не найдена")
 
 
 if __name__ == "__main__":
