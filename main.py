253a931f99ac5f81c00590b8bd86e5220117a42b
TopAcademy <dfghj@sdjh.com>
2024-02-26 21:32:17 +0300

    еще чет добавил, но хз

--- main.py
+++ main.py
@@ -1,60 +1,78 @@
-from flask import Flask, render_template, request, flash, session, redirect, url_for, abort
+import sqlite3
+import os
+from flask import Flask, render_template, request, g, flash, abort
+from usefull.FDataBase import FDataBase
 
 app = Flask(__name__)
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'test.db')))
+app.config["SECRET_KEY"] = "hoirjghojropgjehueEFGEOKOPje"
 
 
-app.config["SECRET_KEY"] = "3SAgu6jDn-u3x6nIEPmf5lTS5"
-menu = [{"name": "Главная", "url": "/"},
-        {"name": "О нас", "url": "/about"},
-        {"name": "Обратная связь", "url": "/contact"},
-        # {"name": "Логин", "url": "/login"},
-        ]
+def connect_db():
+    conn = sqlite3.connect(app.config["DATABASE"])
+    conn.row_factory = sqlite3.Row
+    return conn
 
 
+def create_db():
+    db = connect_db()
+    with app.open_resource("sql_db.sql", mode="r") as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+
+# Routes
 @app.route("/")
-@app.route("/index")
 def index():
-    return render_template("index.html", menu=menu, title="Главгая страница")
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template("index.html", menu=dbase.getMenu(), posts=dbase.getPostAnonce())
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html", menu=menu, title="Страница о компании")
+@app.route("/add_post", methods=["GET", "POST"])
+def addPost():
+    db = get_db()
+    dbase = FDataBase(db)
 
-
-@app.route("/profile/<username>")
-def profile(username):
-    if 'userLogged' not in session['userLogged'] != username:
-        abort(401)
-    return f"<h1> Пользователь: {username} </h1>"
-
-
-@app.route('/contact', methods=["POST", "GET"])
-def contact():
-    if request.method == 'POST':
-        if len(request.form['name']) >= 2:
-            flash("Сообщение доставлено", category="success")
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'], request.form['url'])
+            if not res:
+                flash("Ошибка добавления", category="error")
+            else:
+                flash("Успешно добавлено", category="success")
         else:
-            flash("Ошибка!", category="error")
-    return render_template("contact.html", menu=menu, title="Обратная связь")
+            flash("Ошибка добавления , проверьте ваши данные", category="error")
+
+    return render_template("add_post.html", menu=dbase.getMenu(), title="Добавление статьи")
+
+
+@app.route("/post/<alias>")
+def showPost(alias):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.getPost(alias)
+    if not title:
+        abort(404)
+    return render_template("post.html", menu=dbase.getMenu(), title=title, post=post)
+
+
+def get_db():
+    if not hasattr(g, "link_db"):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, "link_db"):
+        g.link_db.close()
 
 
 @app.errorhandler(404)
 def pageNotFound(error):
-    return render_template("page404.html", menu=menu, title="Страница не найдена")
-
-
-@app.route("/login", methods=["POST", "GET"])
-def login():
-    if 'userLogged' in session:
-        return redirect(url_for('profile', username=session["userLogged"]))
-    elif request.method == "POST":
-        if request.form['name'] == 'Test' and request.form['psw'] == "123":
-            session['userLogged'] = request.form['name']
-            return redirect(url_for('profile', username=session["userLogged"]))
-        else:
-            flash("Ошибка авторизации!")
-    return render_template("login.html", menu=menu, title="Логин")
+    return render_template("page404.html", title="Страница не найдена")
 
 
 if __name__ == "__main__":
